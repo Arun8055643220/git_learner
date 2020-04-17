f6f78c81f0ae942b1e1410a04315bffc1a9622e9
Arun8055643220 <mutkule.arun@gmail.com>
2020-04-17 13:47:57 +0530

    first programmer

---
+++ src/main/java/test/santosh/SpringBootSecurityFacebookLoginOauthApplication.java
@@ -0,0 +1,15 @@
+package test.santosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringBootSecurityFacebookLoginOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFacebookLoginOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/santosh/SpringBootSecurityFacebookLoginOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package test.santosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityFacebookLoginOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
